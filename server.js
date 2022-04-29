b0b6af99387d87d4c7fdad417054a4a3d2163c2a
kayodegeorge <gkayode75@gmail.com>
2022-04-29 12:31:26 +0100

    seond commit

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+const connection = require("./db")
+const user = require(',/routes/users')
 const express = require('express')
 const dotenv = require('dotenv').config()
  const port = process.env.PORT || 7000
 const app = express()
 
-app.get('/api/register', (req,res)=>{
-res.status(200).json({message:'signed up'})
-})
+(async () => await connection())()
+
+app.use(express.json())
+app.use("/api/user", user)
+
+// wait
+// app.get('/api/register', (req,res)=>{
+// res.status(200).json({message:'signed up'})
+// })
+
+
 
 // app.use('/api/goals', require('./routes/goalRoutes'))
 app.listen(port,()=>console.log(`server started on ${port}`))
